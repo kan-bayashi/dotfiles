326f8145a1f468d91e4276ac1e74900f4f9667ff
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-03-16 17:37:20 +0900

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -9,18 +9,14 @@ for f in .??*; do
     [[ "$f" == ".gitignore" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
     [[ "$f" == ".travis.yml" ]] && continue
-    [ -e ~/"$f" ] && mv ~/"$f" ~/"$f".bak
+    [ -e ~/"$f" ] && cp ~/"$f" ~/"$f".bak && rm -f ~/"$f"
     ln -s "$PWD"/"$f" ~/
     echo "Made symlink of $f"
 done
 
 # make symbolic link of vim setting files
-if [ -e ~/.vim/rc ]; then
-    mv ~/.vim ~/.vim.bak
-fi
-if [ -e ~/.config/nvim ]; then
-    mv ~/.config/nvim ~/.config/nvim.bak
-fi
+[ -e ~/.vim ] && cp -r ~/.vim ~/.vim.bak && rm -rf ~/.vim
+[ -e ~/.config/nvim ] && cp -r ~/.config/nvim ~/.config/nvim.bak && rm -rf ~/.config/nvim
 [ ! -e ~/.vim/rc ] && mkdir -p ~/.vim/rc
 [ ! -e ~/.config ] && mkdir -p ~/.config
 ln -s "${PWD}"/vim/dein.toml ~/.vim/rc
@@ -30,20 +26,15 @@ ln -s ~/.vimrc ~/.config/nvim/init.vim
 echo "Made symlink of vim related files."
 
 # copy cool zsh theme
-if [ ! -e ~/.zsh/themes/bullet-train.zsh-theme ];then
-    mkdir -p ~/.zsh/themes
-    ln -s "$PWD"/themes/bullet-train.zsh-theme ~/.zsh/themes
-fi
+[ -e ~/.zsh/themes/bullet-train.zsh-theme ] && \
+    cp ~/.zsh/themes/bullet-train.zsh-theme ~/.zsh/themes/bullet-train.zsh-theme.bak && rm -f ~/.zsh/themes/bullet-train.zsh-theme
+[ ! -e ~/.zsh/themes ] && mkdir -p ~/.zsh/themes
+ln -s "$PWD"/themes/bullet-train.zsh-theme ~/.zsh/themes
 
 # make symbolic link of ipython setting file
-[ ! -e ~/.ipython/profile_default ] && mkdir -p ~/.ipython/profile_default
-[ -e ~/.ipython/profile_default/ipython_config.py ] && \
-    mv ~/.ipython/profile_default/ipython_config.py ~/.ipython/profile_default/ipython_config.py.bak
-ln -s "${PWD}"/ipython/ipython_config.py ~/.ipython/profile_default/
-
+[ -e ~/.ipython/profile_default ] && cp -r ~/.ipython/profile_default ~/.ipython/profile_default.bak
 [ ! -e ~/.ipython/profile_default/startup ] && mkdir -p ~/.ipython/profile_default/startup
-[ -e ~/.ipython/profile_default/startup/keybidings.py ] && \
-    mv ~/.ipython/profile_default/startup/keybidings.py ~/.ipython/profile_default/startup/keybidings.py.bak
+ln -s "${PWD}"/ipython/ipython_config.py ~/.ipython/profile_default
 ln -s "${PWD}"/ipython/startup/keybindings.py ~/.ipython/profile_default/startup
 echo "Made symlink of ipython setting files."
 
