f247de9a5857461c6e72ae8278d690815a50ae49
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-03-16 16:11:04 +0900

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,27 +1,33 @@
-#!/bin/bash
+#!/bin/bash -e
 
-# Hyper cool terminal setup script in takedalab's servers
-# Copyright 2017 Tomoki Hayashi
-set -e
+# Dotfile setup script
+# Copyright 2019 Tomoki Hayashi
 
 # make symbolic link of dotfiles
 for f in .??*; do
     [[ "$f" == ".git" ]] && continue
     [[ "$f" == ".gitignore" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
+    [[ "$f" == ".travis.yml" ]] && continue
     [ -e ~/"$f" ] && mv ~/"$f" ~/"$f".bak
     ln -s "$PWD"/"$f" ~/
-    echo "make symlink of $f"
+    echo "Made symlink of $f"
 done
 
 # make symbolic link of vim setting files
-if [ ! -e ~/.vim/rc ]; then
-    mkdir -p ~/.vim/rc
-    ln -s "$PWD"/vim/dein.toml ~/.vim/rc
-    ln -s "$PWD"/vim/dein_lazy.toml ~/.vim/rc
-    ln -s ~/.vim ~/.config/nvim
-    ln -s ~/.vimrc ~/.config/nvim/init.vim
+if [ -e ~/.vim/rc ]; then
+    mv ~/.vim ~/.vim.bak
 fi
+if [ -e ~/.config/nvim ]; then
+    mv ~/.config/nvim ~/.config/nvim.bak
+fi
+[ ! -e ~/.vim/rc ] && mkdir -p ~/.vim/rc
+[ ! -e ~/.config ] && mkdir -p ~/.config
+ln -s "${PWD}"/vim/dein.toml ~/.vim/rc
+ln -s "${PWD}"/vim/dein_lazy.toml ~/.vim/rc
+ln -s ~/.vim ~/.config/nvim
+ln -s ~/.vimrc ~/.config/nvim/init.vim
+echo "Made symlink of vim related files."
 
 # copy cool zsh theme
 if [ ! -e ~/.zsh/themes/bullet-train.zsh-theme ];then
@@ -29,15 +35,17 @@ if [ ! -e ~/.zsh/themes/bullet-train.zsh-theme ];then
     ln -s "$PWD"/themes/bullet-train.zsh-theme ~/.zsh/themes
 fi
 
-# make symbolic link of vim setting files
-if [ ! -e ~/.ipython/profile_default ];then
-    mkdir -p ~/.ipython/profile_default
-    ln -s "$PWD"/ipython/ipython_config.py ~/.ipython/profile_default/
-fi
-if [ ! -e ~/.ipython/profile_default/startup ];then
-    mkdir -p ~/.ipython/profile_default/startup
-    ln -s "$PWD"/ipython/startup/keybindings.py ~/.ipython/profile_default/startup
-fi
+# make symbolic link of ipython setting file
+[ ! -e ~/.ipython/profile_default ] && mkdir -p ~/.ipython/profile_default
+[ -e ~/.ipython/profile_default/ipython_config.py ] && \
+    mv ~/.ipython/profile_default/ipython_config.py ~/.ipython/profile_default/ipython_config.py.bak
+ln -s "${PWD}"/ipython/ipython_config.py ~/.ipython/profile_default/
+
+[ ! -e ~/.ipython/profile_default/startup ] && mkdir -p ~/.ipython/profile_default/startup
+[ -e ~/.ipython/profile_default/startup/keybidings.py ] && \
+    mv ~/.ipython/profile_default/startup/keybidings.py ~/.ipython/profile_default/startup/keybidings.py.bak
+ln -s "${PWD}"/ipython/startup/keybindings.py ~/.ipython/profile_default/startup
+echo "Made symlink of ipython setting files."
 
 # install zplug
 if [ ! -e ~/.zplug ];then
