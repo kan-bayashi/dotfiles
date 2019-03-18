6265d1072281e0bf01606201aa1b08558df07b1c
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-03-18 21:48:49 +0900

    separate setup and install

---
+++ install.sh
@@ -0,0 +1,62 @@
+#!/bin/bash -e
+
+# Dotfile install script
+# Copyright 2019 Tomoki Hayashi
+
+PYTHON3_VERSION=3.6.6
+
+# check brew installation
+if ! command -v brew > /dev/null; then
+    echo "brew is not installed. please run following command to install brew." 2>&1
+    echo '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"' 2>&1
+    exit 1
+fi
+
+# install zplug
+if [ ! -e ~/.zplug ];then
+    git clone https://github.com/zplug/zplug ~/.zplug
+fi
+
+# install tpm
+if [ ! -e ~/.tmux/plugins/tpm ];then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+# install fzf
+if [ ! -e ~/.fzf ];then
+    workdir=$(pwd)
+    git clone https://github.com/junegunn/fzf.git ~/.fzf
+    cd ~/.fzf && ./install && cd ${workdir}
+fi
+
+# install pyenv
+if [ ! -e ~/.pyenv ];then
+    git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+fi
+
+# install essential tools
+for tool in zsh fd ripgrep neovim tmux; do
+    if brew ls --versions ${tool} > /dev/null; then brew upgrade ${tool}; else brew install ${tool}; fi
+done
+
+# install pyenv python3.6
+if [ ! -e ~/.pyenv/versions/${PYTHON3_VERSION} ];then
+    # install dependencies for pyenv python installation
+    for tool in readline xz; do
+        if brew ls --versions ${tool} > /dev/null; then brew upgrade ${tool}; else brew install ${tool}; fi
+    done
+    brew install --upgrade readline xz
+    mac_version=$(sw_vers | grep ProductVersion | awk '{print $2}' | sed -e "s/\.[0-9]$//g")
+    # need to install extra modules
+    # https://github.com/pyenv/pyenv/wiki/common-build-problems
+    [ $(echo "${mac_version} >= 10.14" | bc) -eq 1 ] && \
+        sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
+
+    # install python3 through pyenv
+    export PATH=$HOME/.pyenv/bin:$PATH
+    eval "$(pyenv init -)"
+    env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install ${PYTHON3_VERSION}
+    pyenv global ${PYTHON3_VERSION}
+    ~/.pyenv/shims/pip install --upgrade pip
+    ~/.pyenv/shims/pip install -r requirements.txt
+fi
--- setup.sh
+++ setup.sh
@@ -39,46 +39,5 @@ ln -s "${PWD}"/ipython/ipython_config.py ~/.ipython/profile_default
 ln -s "${PWD}"/ipython/startup/keybindings.py ~/.ipython/profile_default/startup
 echo "Made symlink of ipython setting files."
 
-# install zplug
-if [ ! -e ~/.zplug ];then
-    echo "####################################################"
-    echo "#                    ZPLUG INSTALL                 #"
-    echo "####################################################"
-    git clone https://github.com/zplug/zplug ~/.zplug
-fi
-
-# install tpm
-if [ ! -e ~/.tmux/plugins/tpm ];then
-    echo "####################################################"
-    echo "#           TMUX PLUGIN MANAGER INSTALL            #"
-    echo "####################################################"
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-fi
-
-# install fzf
-if [ ! -e ~/.fzf ];then
-    echo "####################################################"
-    echo "#                   FZF INSTALL                    #"
-    echo "####################################################"
-    git clone https://github.com/junegunn/fzf.git ~/.fzf
-    cd ~/.fzf && ./install
-fi
-
-# install pyenv
-if [ ! -e ~/.pyenv ];then
-    echo "####################################################"
-    echo "#                 PYENV INSTALL                    #"
-    echo "####################################################"
-    git clone https://github.com/yyuu/pyenv.git ~/.pyenv
-
-    # install pyenv python3.6
-    export PATH=$HOME/.pyenv/bin:$PATH
-    eval "$(pyenv init -)"
-    env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install 3.6.6
-    pyenv global 3.6.6
-    $HOME/.pyenv/shims/pip install --upgrade pip
-    $HOME/.pyenv/shims/pip install -r requirements.txt
-fi
-
-# start zsh
-exec zsh -l
+echo "Sucessfully setup dotfiles."
+echo "Next, please run install.sh to install essential tools."
