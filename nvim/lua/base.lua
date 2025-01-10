93816b5068c55362a3781179b2a8206f48858c1b
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2025-01-10 15:52:54 +0900

    update

--- nvim/lua/base.lua
+++ nvim/lua/base.lua
@@ -101,81 +101,81 @@ keymap("n", "q:", ":q<CR>", keymap_opts)
 local N = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 
 local function encode_base64(data)
-	local data1 = (
-		data:gsub(
-			".",
-			--- @param x string
-			--- @return string
-			function(x)
-				local r, b = "", x:byte()
-				for i = 8, 1, -1 do
-					r = r .. (b % 2 ^ i - b % 2 ^ (i - 1) > 0 and "1" or "0")
-				end
-				return r
-			end
-		) .. "0000"
-	)
+  local data1 = (
+    data:gsub(
+      ".",
+      --- @param x string
+      --- @return string
+      function(x)
+        local r, b = "", x:byte()
+        for i = 8, 1, -1 do
+          r = r .. (b % 2 ^ i - b % 2 ^ (i - 1) > 0 and "1" or "0")
+        end
+        return r
+      end
+    ) .. "0000"
+  )
 
-	local data2 = data1:gsub(
-		"%d%d%d?%d?%d?%d?",
-		--- @param x string
-		--- @return string
-		function(x)
-			if #x < 6 then
-				return ""
-			end
-			local c = 0
-			for i = 1, 6 do
-				c = c + (x:sub(i, i) == "1" and 2 ^ (6 - i) or 0)
-			end
-			return N:sub(c + 1, c + 1)
-		end
-	)
+  local data2 = data1:gsub(
+    "%d%d%d?%d?%d?%d?",
+    --- @param x string
+    --- @return string
+    function(x)
+      if #x < 6 then
+        return ""
+      end
+      local c = 0
+      for i = 1, 6 do
+        c = c + (x:sub(i, i) == "1" and 2 ^ (6 - i) or 0)
+      end
+      return N:sub(c + 1, c + 1)
+    end
+  )
 
-	local suffix = ({ "", "==", "=" })[#data % 3 + 1]
+  local suffix = ({ "", "==", "=" })[#data % 3 + 1]
 
-	return data2 .. suffix
+  return data2 .. suffix
 end
 
 local function osc52_copy(text)
-	local text_b64 = encode_base64(text)
-	local osc = string.format("%s]52;c;%s%s", string.char(0x1b), text_b64, string.char(0x07))
-	io.stderr:write(osc)
+  local text_b64 = encode_base64(text)
+  local osc = string.format("%s]52;c;%s%s", string.char(0x1b), text_b64, string.char(0x07))
+  io.stderr:write(osc)
 end
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		osc52_copy(vim.fn.getreg(vim.v.event.regname))
-	end,
+  callback = function()
+    osc52_copy(vim.fn.getreg(vim.v.event.regname))
+  end,
 })
 
 -- Autocommands
 local autocmd = vim.api.nvim_create_autocmd
 autocmd("Filetype", {
-	pattern = "yaml",
-	command = "setlocal tabstop=2 softtabstop=2 shiftwidth=2",
+  pattern = "yaml",
+  command = "setlocal tabstop=2 softtabstop=2 shiftwidth=2",
 })
 autocmd("Filetype", {
-	pattern = "tf",
-	command = "setlocal tabstop=2 softtabstop=2 shiftwidth=2",
+  pattern = "tf",
+  command = "setlocal tabstop=2 softtabstop=2 shiftwidth=2",
 })
 autocmd("Filetype", {
-	pattern = "json",
-	command = "set conceallevel=0",
+  pattern = "json",
+  command = "set conceallevel=0",
 })
 autocmd("Filetype", {
-	pattern = "vim",
-	command = "setlocal tabstop=2 softtabstop=2 shiftwidth=2",
+  pattern = "vim",
+  command = "setlocal tabstop=2 softtabstop=2 shiftwidth=2",
 })
 autocmd("Filetype", {
-	pattern = "lua",
-	command = "setlocal tabstop=2 softtabstop=2 shiftwidth=2",
+  pattern = "lua",
+  command = "setlocal tabstop=2 softtabstop=2 shiftwidth=2",
 })
 autocmd("Filetype", {
-	pattern = "typescriptreact",
-	command = "setlocal tabstop=2 softtabstop=2 shiftwidth=2",
+  pattern = "typescriptreact",
+  command = "setlocal tabstop=2 softtabstop=2 shiftwidth=2",
 })
 autocmd("Filetype", {
-	pattern = "gitcommit",
-	command = "setlocal spell",
+  pattern = "gitcommit",
+  command = "setlocal spell",
 })
