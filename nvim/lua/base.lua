bbca70db8f28d52dfd7a801d5efff20d7d68d849
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2025-01-10 16:46:09 +0900

    update

--- nvim/lua/base.lua
+++ nvim/lua/base.lua
@@ -97,35 +97,70 @@ keymap("n", "<Leader>Q", ":bdel!<CR>", keymap_opts)
 -- Preserve annoying pop
 keymap("n", "q:", ":q<CR>", keymap_opts)
 
--- Copy to clipboard
-vim.cmd([[
-  function! Yank(text) abort
-    let escape = system('yank', a:text)
-    if v:shell_error
-      echoerr escape
-    else
-      call writefile([escape], '/dev/tty', 'b')
-    endif
-  endfunction
-]])
-keymap("", "<Leader>y", "y:<C-u>call Yank(@0)<CR>", keymap_opts)
+-- Copy to clipboard via osc52 (ref: https://github.com/neovim/neovim/pull/25872#issuecomment-1790586800)
+local N = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
+local function encode_base64(data)
+  local data1 = (
+    data:gsub(
+      ".",
+      --- @param x string
+      --- @return string
+      function(x)
+        local r, b = "", x:byte()
+        for i = 8, 1, -1 do
+          r = r .. (b % 2 ^ i - b % 2 ^ (i - 1) > 0 and "1" or "0")
+        end
+        return r
+      end
+    ) .. "0000"
+  )
+
+  local data2 = data1:gsub(
+    "%d%d%d?%d?%d?%d?",
+    --- @param x string
+    --- @return string
+    function(x)
+      if #x < 6 then
+        return ""
+      end
+      local c = 0
+      for i = 1, 6 do
+        c = c + (x:sub(i, i) == "1" and 2 ^ (6 - i) or 0)
+      end
+      return N:sub(c + 1, c + 1)
+    end
+  )
+
+  local suffix = ({ "", "==", "=" })[#data % 3 + 1]
+
+  return data2 .. suffix
+end
+
+local function osc52_copy(text)
+  local text_b64 = encode_base64(text)
+  local osc = string.format("%s]52;c;%s%s", string.char(0x1b), text_b64, string.char(0x07))
+  io.stderr:write(osc)
+end
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    osc52_copy(vim.fn.getreg(vim.v.event.regname))
+  end,
+})
 
 -- Autocommands
 local autocmd = vim.api.nvim_create_autocmd
-autocmd("BufNewFile,BufRead", {
-  pattern = "*.yaml",
+autocmd("Filetype", {
+  pattern = "yaml",
   command = "setlocal tabstop=2 softtabstop=2 shiftwidth=2",
 })
-autocmd("BufNewFile,BufRead", {
-  pattern = "*.yml",
+autocmd("Filetype", {
+  pattern = "tf",
   command = "setlocal tabstop=2 softtabstop=2 shiftwidth=2",
 })
-autocmd("BufNewFile,BufRead", {
-  pattern = "*.tf",
-  command = "setlocal tabstop=2 softtabstop=2 shiftwidth=2",
-})
-autocmd("BufNewFile,BufRead", {
-  pattern = "*.json",
+autocmd("Filetype", {
+  pattern = "json",
   command = "set conceallevel=0",
 })
 autocmd("Filetype", {
@@ -134,6 +169,10 @@ autocmd("Filetype", {
 })
 autocmd("Filetype", {
   pattern = "lua",
+  command = "setlocal tabstop=2 softtabstop=2 shiftwidth=2",
+})
+autocmd("Filetype", {
+  pattern = "typescriptreact",
   command = "setlocal tabstop=2 softtabstop=2 shiftwidth=2",
 })
 autocmd("Filetype", {
