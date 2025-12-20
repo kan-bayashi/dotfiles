41ea02231a97eaa1b350a91b03ccad39f8de26cd
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2025-12-20 14:30:11 +0900

    update

--- nvim/lua/plugins/appearance.lua
+++ nvim/lua/plugins/appearance.lua
@@ -5,6 +5,10 @@ return {
     dependencies = { "rktjmp/lush.nvim" },
     config = function()
       vim.cmd([[colorscheme jellybeans]])
+      -- Transparent background
+      vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+      vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
     end,
   },
   -- Start screen
