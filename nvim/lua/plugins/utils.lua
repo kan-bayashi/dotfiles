e65e317f8ac095fdf4594e34faf7acba2b7557df
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2025-12-21 21:59:42 +0900

    update

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -246,4 +246,19 @@ return {
       })
     end,
   },
+  -- Key mapping helper
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
 }
